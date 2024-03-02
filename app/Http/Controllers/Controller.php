f6dbbeace73d05efb8664ff5de0817693bb13183
Tripuwarej <tripuwarej.paw@gmail.com>
2024-03-02 21:36:47 +0700

    add script pusher

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Events\LiveChat;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public  function TestChat(){
+        $text = 'wow';
+        event(new LiveChat($text));
+//        DB::table('log_chat')->insert([
+//            'text' => $text
+//        ]);
+        return 'Send Message Success';
+    }
 }
