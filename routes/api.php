27e1b44f77e4894883f6c3eebd1a2ed57f7828e6
nichakan <nichakan4740@gmail.com>
2024-03-26 04:08:14 +0700

    add_commit

--- routes/api.php
+++ routes/api.php
@@ -88,8 +88,6 @@ Route::put('nurse/getProfile/{id}', [AdminController::class, 'updateuser']);
 
 
     /* chat */
-    /* Route::apiResource('/conversations', ConversationController::class);
- */
     Route::post('/conversations', [ConversationController::class, 'store']);
     /* ตอบกลับข้อความ */
     Route::post('/conversation/reply', [ConversationController::class, 'Adminrespondsuser']); // เส้นทางสำหรับตอบกลับ conversation
